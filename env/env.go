d299748b829c148eb8fe0428d14c5701c75529ba
kiyon <kiyonlin@163.com>
2018-12-24 15:34:06 +0800

    init

---
+++ env/env.go
@@ -0,0 +1,92 @@
+package env
+
+import (
+	"fmt"
+	"path"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+func Load(filePath string) {
+	viper.SetConfigName(path.Base(filePath))
+	viper.AddConfigPath(path.Dir(filePath))
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+}
+
+func GetValue(key string, defaultValue ...interface{}) interface{} {
+	if len(defaultValue) > 0 {
+		viper.SetDefault(key, defaultValue[0])
+	}
+	return viper.Get(key)
+}
+
+func GetBool(key string, defaultValue ...bool) bool {
+	if len(defaultValue) > 0 {
+		viper.SetDefault(key, defaultValue[0])
+	}
+	return viper.GetBool(key)
+}
+
+func GetFloat64(key string, defaultValue ...float64) float64 {
+	if len(defaultValue) > 0 {
+		viper.SetDefault(key, defaultValue[0])
+	}
+	return viper.GetFloat64(key)
+}
+
+func GetInt(key string, defaultValue ...int) int {
+	if len(defaultValue) > 0 {
+		viper.SetDefault(key, defaultValue[0])
+	}
+	return viper.GetInt(key)
+}
+
+func GetString(key string, defaultValue ...string) string {
+	if len(defaultValue) > 0 {
+		viper.SetDefault(key, defaultValue[0])
+	}
+	return viper.GetString(key)
+}
+
+func GetStringMap(key string, defaultValue ...map[string]interface{}) map[string]interface{} {
+	if len(defaultValue) > 0 {
+		viper.SetDefault(key, defaultValue[0])
+	}
+	return viper.GetStringMap(key)
+}
+
+func GetStringMapString(key string, defaultValue ...map[string]string) map[string]string {
+	if len(defaultValue) > 0 {
+		viper.SetDefault(key, defaultValue[0])
+	}
+	return viper.GetStringMapString(key)
+}
+
+func GetStringSlice(key string, defaultValue ...[]string) []string {
+	if len(defaultValue) > 0 {
+		viper.SetDefault(key, defaultValue[0])
+	}
+	return viper.GetStringSlice(key)
+}
+
+func GetTime(key string, defaultValue ...time.Time) time.Time {
+	if len(defaultValue) > 0 {
+		viper.SetDefault(key, defaultValue[0])
+	}
+	return viper.GetTime(key)
+}
+
+func GetDuration(key string, defaultValue ...time.Duration) time.Duration {
+	if len(defaultValue) > 0 {
+		viper.SetDefault(key, defaultValue[0])
+	}
+	return viper.GetDuration(key)
+}
+
+func AllSettings() map[string]interface{} {
+	return viper.AllSettings()
+}
